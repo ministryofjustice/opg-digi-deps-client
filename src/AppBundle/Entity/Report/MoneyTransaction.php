a4393d1e219ac0a8c7d77669aab6e1f8f6ca26fd
Shaun Lizzio <shaun@lizzio.co.uk>
2018-03-23 16:25:08 +0000

    DDPB-1849 Updated with config new sections

--- src/AppBundle/Entity/Report/MoneyTransaction.php
+++ src/AppBundle/Entity/Report/MoneyTransaction.php
@@ -153,13 +153,13 @@ class MoneyTransaction
             ],
             'config' => ['hasDetails' => false, 'type' => 'out']
         ],
-        'moving-money' => [
-            'categories' => [
-                'cash-withdrawn' => ['config' => ['hasDetails' => true, 'type' => 'out']],
-                'transfers-out-to-other-accounts' => ['config' => ['hasDetails' => true, 'type' => 'out']],
-                'anything-else-paid-out' => ['config' => ['hasDetails' => true, 'type' => 'out']],
-            ],
-            'config' => ['hasDetails' => false, 'type' => 'out']
+        'cash-withdrawn' => [
+            'categories' => [],
+            'config' => ['hasDetails' => true, 'type' => 'out']
+        ],
+        'transfers-out-to-other-accounts' => [
+            'categories' => [],
+            'config' => ['hasDetails' => true, 'type' => 'out']
         ],
         'moneyout-other' => [
             'categories' => [
