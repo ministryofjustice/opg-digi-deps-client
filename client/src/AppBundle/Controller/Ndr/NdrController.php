c1651035cf93ee27b8a9d5dd35190c85fbe54ca2
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-07-30 08:49:57 +0100

    Use standalone NDR template for PDF generation

--- client/src/AppBundle/Controller/Ndr/NdrController.php
+++ client/src/AppBundle/Controller/Ndr/NdrController.php
@@ -143,17 +143,9 @@ class NdrController extends AbstractController
 
     private function getPdfBinaryContent($ndr)
     {
-        $html = $this->render('AppBundle:Ndr/Formatted:formatted_body.html.twig', [
+        $html = $this->render('AppBundle:Ndr/Formatted:formatted_standalone.html.twig', [
             'ndr' => $ndr, 'adLoggedAsDeputy' => $this->isGranted(User::ROLE_AD)
         ])->getContent();
-
-        // Insert formatted report CSS
-        $assetPath = __DIR__ . '/../../../../web/assets/';
-        $assetContents = array_diff(scandir($assetPath), ['..', '.']);
-        $folderName = array_values($assetContents)[0];
-
-        $css = file_get_contents($assetPath . $folderName . '/stylesheets/formatted-report.css');
-        $html = '<style type="text/css" media="screen, print">' . $css . '</style>' . $html;
 
         return $this->get('wkhtmltopdf')->getPdfFromHtml($html);
     }
